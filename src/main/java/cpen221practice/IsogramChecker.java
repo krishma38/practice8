00ef12273430e8d03c2d39e7df04bccfaf542845
krishma <krishmas536@gmail.com>
2021-10-30 17:43:17 -0700

    Done?

--- src/main/java/cpen221practice/IsogramChecker.java
+++ src/main/java/cpen221practice/IsogramChecker.java
@@ -3,7 +3,25 @@ package cpen221practice;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        phrase = phrase.toLowerCase();
+        phrase = phrase.replaceAll(" ", "");
+        phrase = phrase.replaceAll("-", "");
+
+        if(phrase.length() == 1){
+            return true;
+        }
+        for (int i = 0; i < phrase.length(); i++) {
+            for (int j = i + 1; j < phrase.length(); j++) {
+
+                if(phrase.charAt(i) == phrase.charAt(j)){
+                    return false;
+                }
+            }
+
+        }
+
+     return true;
     }
 
 }
